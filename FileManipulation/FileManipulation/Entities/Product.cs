e24e963068e31b7644c9a7918769aa55a50c625f
eliasdaniellopes <eliasdaniellopes94@gmail.com>
2019-02-04 13:53:03 -0200

    Manipulação de arquivo csv

---
+++ FileManipulation/FileManipulation/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace FileManipulation.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public double Preco()
+        {
+            return Price * Quantity;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name}, {Preco().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ FileManipulation/FileManipulation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using FileManipulation.Entities;
+using System.Globalization;
+namespace FileManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = (@"C:\Users\~\Documents\csv\produtos.csv");
+            string target = (@"C:\Users\~\Documents\csv\relatorios.csv");
+
+            try
+            {
+                using (StreamWriter sw = File.AppendText(target))
+                {
+                    string[] lines = File.ReadAllLines(path);
+
+
+                    foreach (string line in lines)
+                    {
+                        string[] field = line.Split(",");
+                        string name = field[0];
+                        double price = double.Parse(field[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(field[2]);
+                        Product p = new Product(name, price, quantity);
+
+                        sw.WriteLine(p);
+                    }
+                }
+            }catch(IOException e)
+            {
+                Console.WriteLine("Error " + e.Message);
+            }
+        }
+    }
+}
